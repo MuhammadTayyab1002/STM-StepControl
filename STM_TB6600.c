9b44dd41490de3b9ae11159a71dc5d8aac9cb827
Muhammad Tayyab <m.tayyabtahir1002@gmail.com>
2024-03-14 09:50:01 +0500

    More Generic

--- STM_TB6600.c
+++ STM_TB6600.c
@@ -2,8 +2,59 @@
 #include "STM_TB6600Config.h"
 #include "main.h"
 #include "math.h"
-//#include "stm32f0xx_hal_tim.h"
-#include "stm32l4xx_hal_tim.h"
+/* --------------- Check Mainstream series --------------- */
+
+	#ifdef STM32F0
+		#include "stm32f0xx_hal.h"       /* Import HAL library */
+		#include "stm32f0xx_hal_tim.h" 
+	#elif defined(STM32F1)
+		#include "stm32f1xx_hal.h"       /* Import HAL library */
+		#include "stm32f1xx_hal_tim.h"
+	#elif defined(STM32F2)
+		#include "stm32f2xx_hal.h"       /* Import HAL library */
+		#include "stm32f2xx_hal_tim.h"
+	#elif defined(STM32F3)
+		#include "stm32f3xx_hal.h"       /* Import HAL library */
+		#include "stm32f3xx_hal_tim.h"
+	#elif defined(STM32F4)
+		#include "stm32f4xx_hal.h"       /* Import HAL library */
+		#include "stm32f4xx_hal_tim.h"
+	#elif defined(STM32F7)
+		#include "stm32f7xx_hal.h"       /* Import HAL library */
+		#include "stm32f7xx_hal_tim.h"
+	#elif defined(STM32G0)
+		#include "stm32g0xx_hal.h"       /* Import HAL library */
+		#include "stm32g0xx_hal_tim.h"
+	#elif defined(STM32G4)
+		#include "stm32g4xx_hal.h"       /* Import HAL library */
+		#include "stm32g4xx_hal_tim.h"
+
+	/* ------------ Check High Performance series ------------ */
+
+	#elif defined(STM32H7)
+		#include "stm32h7xx_hal.h"       /* Import HAL library */
+		#include "stm32h7xx_hal_tim.h"
+
+	/* ------------ Check Ultra low power series ------------- */
+
+	#elif defined(STM32L0)
+		#include "stm32l0xx_hal.h"       /* Import HAL library */
+		#include "stm32l0xx_hal_tim.h"
+	#elif defined(STM32L1)
+		#include "stm32l1xx_hal.h"       /* Import HAL library */0
+		#include "stm32l1xx_hal_tim.h"
+	#elif defined(STM32L5)
+		#include "stm32l5xx_hal.h"       /* Import HAL library */
+		#include "stm32l5xx_hal_tim.h"
+	#elif defined(STM32L4)
+		#include "stm32l4xx_hal.h"       /* Import HAL library */
+		#include "stm32l4xx_hal_tim.h"
+	#elif defined(STM32H7)
+		#include "stm32h7xx_hal.h"       /* Import HAL library */
+		#include "stm32h7xx_hal_tim.h"
+	#else
+	#endif /* STM32F1 */
+uint16_t count;
 //#############################################################################################
  
 //#############################################################################################
@@ -30,12 +81,25 @@ void tb6600_init(tb6600_t *tb6600, GPIO_TypeDef  *enable_gpio, uint16_t enable_p
 //#############################################################################################
 
 //#############################################################################################
+void tb6600_init_tim(tb6600_t *tb6600, GPIO_TypeDef  *enable_gpio, uint16_t enable_pin, GPIO_TypeDef  *direction_gpio, uint16_t direction_pin,TIM_HandleTypeDef timer,uint32_t Channel)
+{
+	tb6600->enable_gpio = enable_gpio;
+	tb6600->enable_pin = enable_pin;
+	tb6600->direction_gpio = direction_gpio;
+	tb6600->direction_pin = direction_pin;
+	tb6600->timer=timer;
+	tb6600->Channel=Channel;
+}
+//#############################################################################################
+
+
+//#############################################################################################
 void tb6600_Set(tb6600_t *tb6600,Direction direction,Speed speed,uint16_t PulsePerRev,uint16_t Clock)
 {
 	if(direction==CW)
-	{HAL_GPIO_WritePin(tb6600->direction_gpio,tb6600->direction_pin,GPIO_PIN_SET);}
+	{HAL_GPIO_WritePin(tb6600->direction_gpio,tb6600->direction_pin,GPIO_PIN_RESET);}
 	else if(direction==CCW)
-	{HAL_GPIO_WritePin(tb6600->direction_gpio,tb6600->direction_pin,0);}
+	{HAL_GPIO_WritePin(tb6600->direction_gpio,tb6600->direction_pin,GPIO_PIN_SET);}
 	tb6600->PulsePerRev=PulsePerRev;
 	tb6600->Speed=speed;
 	__HAL_TIM_SET_PRESCALER(&tb6600->timer,Clock-1);
@@ -73,9 +137,9 @@ void tb6600_Disable(tb6600_t *tb6600)
 void tb6600_ChangeDirection(tb6600_t *tb6600,Direction direction)
 {
 	if(direction==CW)
-	{HAL_GPIO_WritePin(tb6600->direction_gpio,tb6600->direction_pin,GPIO_PIN_SET);}
-	else if(direction==CCW)
 	{HAL_GPIO_WritePin(tb6600->direction_gpio,tb6600->direction_pin,GPIO_PIN_RESET);}
+	else if(direction==CCW)
+	{HAL_GPIO_WritePin(tb6600->direction_gpio,tb6600->direction_pin,GPIO_PIN_SET);}
 }
 //#############################################################################################
 
@@ -130,6 +194,15 @@ void tb6600_cont(tb6600_t *tb6600)
 //#############################################################################################
 
 //#############################################################################################
+void tb6600_cont_tim(tb6600_t *tb6600)
+{
+	tb6600->timer.Instance->ARR=(tb6600->Speed*2);
+	tb6600->timer.Instance->CCR1=(tb6600->Speed);
+	HAL_TIM_PWM_Start(&tb6600->timer,tb6600->Channel);
+}
+//#############################################################################################
+
+//#############################################################################################
 void tb6600_Movemm(tb6600_t *tb6600,uint16_t Millimeters)
 {
 	int Degree=(Millimeters/7.5)*360; // motor moves 7.5mm per 360 degree for nema 17 on a linear guide whose dia is 8mm
@@ -141,4 +214,16 @@ void tb6600_Movemm(tb6600_t *tb6600,uint16_t Millimeters)
 		delay_us(tb6600);   
 	}
 }
-//#############################################################################################v
+//#############################################################################################
+
+//#############################################################################################
+void tb6600_Stop(tb6600_t *tb6600)
+{
+	HAL_GPIO_WritePin(tb6600->pulse_gpio,tb6600->pulse_pin,GPIO_PIN_RESET);
+}
+//#############################################################################################
+
+void HAL_TIM_PWM_PulseFinishedCallback(TIM_HandleTypeDef *htim)
+{
+	
+}
--- STM_TB6600.h
+++ STM_TB6600.h
@@ -18,10 +18,58 @@ extern "C" {
 #endif
   
 #include "main.h"
-//#include "stm32f0xx_hal.h"
-//#include "stm32f0xx_hal_tim.h"
-#include "stm32l4xx_hal_tim.h"
-#include "stm32l4xx_hal.h"
+/* --------------- Check Mainstream series --------------- */
+
+	#ifdef STM32F0
+		#include "stm32f0xx_hal.h"       /* Import HAL library */
+		#include "stm32f0xx_hal_tim.h" 
+	#elif defined(STM32F1)
+		#include "stm32f1xx_hal.h"       /* Import HAL library */
+		#include "stm32f1xx_hal_tim.h"
+	#elif defined(STM32F2)
+		#include "stm32f2xx_hal.h"       /* Import HAL library */
+		#include "stm32f2xx_hal_tim.h"
+	#elif defined(STM32F3)
+		#include "stm32f3xx_hal.h"       /* Import HAL library */
+		#include "stm32f3xx_hal_tim.h"
+	#elif defined(STM32F4)
+		#include "stm32f4xx_hal.h"       /* Import HAL library */
+		#include "stm32f4xx_hal_tim.h"
+	#elif defined(STM32F7)
+		#include "stm32f7xx_hal.h"       /* Import HAL library */
+		#include "stm32f7xx_hal_tim.h"
+	#elif defined(STM32G0)
+		#include "stm32g0xx_hal.h"       /* Import HAL library */
+		#include "stm32g0xx_hal_tim.h"
+	#elif defined(STM32G4)
+		#include "stm32g4xx_hal.h"       /* Import HAL library */
+		#include "stm32g4xx_hal_tim.h"
+
+	/* ------------ Check High Performance series ------------ */
+
+	#elif defined(STM32H7)
+		#include "stm32h7xx_hal.h"       /* Import HAL library */
+		#include "stm32h7xx_hal_tim.h"
+
+	/* ------------ Check Ultra low power series ------------- */
+
+	#elif defined(STM32L0)
+		#include "stm32l0xx_hal.h"       /* Import HAL library */
+		#include "stm32l0xx_hal_tim.h"
+	#elif defined(STM32L1)
+		#include "stm32l1xx_hal.h"       /* Import HAL library */0
+		#include "stm32l1xx_hal_tim.h"
+	#elif defined(STM32L5)
+		#include "stm32l5xx_hal.h"       /* Import HAL library */
+		#include "stm32l5xx_hal_tim.h"
+	#elif defined(STM32L4)
+		#include "stm32l4xx_hal.h"       /* Import HAL library */
+		#include "stm32l4xx_hal_tim.h"
+	#elif defined(STM32H7)
+		#include "stm32h7xx_hal.h"       /* Import HAL library */
+		#include "stm32h7xx_hal_tim.h"
+	#else
+	#endif /* STM32F1 */
 //####################################################################################################################
 typedef struct
 {
@@ -34,13 +82,14 @@ typedef struct
 	uint16_t      pulse_pin;
 	uint16_t			PulsePerRev;
 	uint16_t			Speed;
+	uint32_t      Channel;
 }tb6600_t;
 
 
 typedef enum
     {
-				CW = GPIO_PIN_SET,  ///< Counter-Clockwise
-        CCW  = GPIO_PIN_RESET  ///< Clockwise
+				CCW = 1,  ///< Counter-Clockwise
+        CW  = 0  ///< Clockwise
     } Direction;
 
 typedef enum
@@ -55,6 +104,7 @@ typedef enum
 //####################################################################################################################
 		
 void tb6600_init(tb6600_t *tb6600, GPIO_TypeDef  *enable_gpio, uint16_t enable_pin, GPIO_TypeDef  *direction_gpio, uint16_t direction_pin,GPIO_TypeDef  *pulse_gpio,uint16_t pulse_pin,TIM_HandleTypeDef htimx);
+void tb6600_init_tim(tb6600_t *tb6600, GPIO_TypeDef  *enable_gpio, uint16_t enable_pin, GPIO_TypeDef  *direction_gpio, uint16_t direction_pin,TIM_HandleTypeDef timer,uint32_t Channel);
 void tb6600_Set(tb6600_t *tb6600,Direction direction,Speed speed,uint16_t PulsePerRev,uint16_t Clock);
 void tb6600_AngleMove(tb6600_t *tb6600,uint16_t Degree);
 void tb6600_Enable(tb6600_t *tb6600);
@@ -63,9 +113,11 @@ void tb6600_ChangeDirection(tb6600_t *tb6600,Direction direction);
 void tb6600_ChangeSpeed(tb6600_t *tb6600,Speed speed);
 void tb6600_ChangePulsePerRev(tb6600_t *tb6600,uint16_t PulsePerRev);
 void tb6600_cont(tb6600_t *tb6600);
+void tb6600_cont_tim(tb6600_t *tb6600);
 void tb6600_Movemm(tb6600_t *tb6600,uint16_t Millimeters);
 void tb6600_ManualSpeed(tb6600_t *tb6600,uint16_t mmpersec);
-		
+void tb6600_Stop(tb6600_t *tb6600);
+
 //####################################################################################################################
 		
 #endif
